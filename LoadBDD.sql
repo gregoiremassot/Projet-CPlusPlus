721c66cf5e08ec47b52caa5540f4e322175e342f
Grégoire MASSOT <gregoire.massot@orange.fr>
2018-05-06 04:07:00 +0200

    Modification SQL - Load tables + export TXT

--- LoadBDD.sql
+++ LoadBDD.sql
@@ -31,7 +31,50 @@ CREATE TABLE suivre
 (
   code_cours INT,
   numero_etudiant INT,
-  note INT,
-  PRIMARY KEY(code_cours, numero_etudiant)
+  note INT
+  -- PRIMARY KEY(code_cours, numero_etudiant)
 );
+
+LOAD DATA INFILE 'C:/projetcpp/projetcpp/BDD/Enseignant.csv'
+INTO TABLE enseignant
+FIELDS TERMINATED BY ';'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+SELECT * FROM enseignant;
+
+LOAD DATA INFILE 'C:/projetcpp/projetcpp/BDD/Etudiant.csv'
+INTO TABLE etudiant
+FIELDS TERMINATED BY ';'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+SELECT * FROM etudiant;
+
+LOAD DATA INFILE 'C:/projetcpp/projetcpp/BDD/Cours.csv'
+INTO TABLE cours
+FIELDS TERMINATED BY ';'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+SELECT * FROM cours;
+
+LOAD DATA INFILE 'C:/projetcpp/projetcpp/BDD/Notes.csv'
+INTO TABLE suivre
+FIELDS TERMINATED BY ';'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+SELECT * FROM suivre;
+
+DELETE t1 FROM suivre t1
+INNER JOIN suivre t2
+WHERE t1.numero_etudiant = t2.numero_etudiant
+AND t1.code_cours = t2.code_cours
+AND t1.note < t2.note;
+
+SELECT enseignant.numero, enseignant.nom, GROUP_CONCAT(cours.code SEPARATOR ';')
+FROM enseignant INNER JOIN cours ON enseignant.numero = cours.numero_enseignant
+GROUP BY enseignant.numero, enseignant.nom;
+
 
