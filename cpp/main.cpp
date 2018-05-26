d18dbecf454b02d172bba495f31f66f54408e625
Grégoire MASSOT <gregoire.massot@orange.fr>
2018-05-26 12:19:15 +0200

    Ajout du menu

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,6 +1,8 @@
 // Binome : Grégoire MASSOT - David-Anthony SELLEM
 #include <iostream>
 #include "../h/Universite.h"
+
+using namespace std;
 int main()
 {
     /*Cours Biologie("2");
@@ -13,6 +15,32 @@ int main()
     cout << DavidAnthony.moyenne()<< endl;*/
 
     Universite ISFA("Universite");
+    int continuer = 1;
+    int choix;
+
+    while(continuer == 1)
+    {
+        cout << "1- Afficher la liste des cours | 2- ...";
+        cin >> choix;
+        switch(choix)
+        {
+            case 1:
+                cout << "Choix 1";
+                break;
+
+            case 2:
+                cout << "Choix 2";
+                break;
+
+            case 3:
+                cout << "Choix 3";
+                break;
+
+            default :
+                continuer = 0;
+                break;
+        }
+    }
 
     //cout << ISFA.getCours(8)->moyenne() << endl;
     //ISFA.getCours(8)->help_cours();
@@ -20,7 +48,7 @@ int main()
     //ISFA.getEtudiantMaster(35)->getTutores();
     //ISFA.getCours(8)->getClassementCours();
     //ISFA.getEnseignant(2)->afficherEtudiantDifficulte();
-    //ISFA.getEtudiant(2)->getClassementCours(7);
-    
+    ISFA.getEtudiant(2)->getClassementCours(7);
+
     return 0;
 }
